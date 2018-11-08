c0b44fd104ac5bf85484699bd0c54a1ce02b3df3
nerd553 <geomelkon@csd.auth.gr>
2018-11-08 12:12:33 +0200

    Removed unnecessary animation

--- scripts/biography.js
+++
@@ -1,9 +0,0 @@
-const siffredi = document.getElementById("siffredi-costume");
-//const background = document.getElementsByClassName("stats")[0];
-
-function moveBackground(event) {
-    const position = -event.clientX / 80;
-    //siffredi.style.marginRight = position+"px";
-    siffredi.style.transform = "rotateY("+position+"deg)";
-    //siffredi.style.filter = "blur("+(-position/20)+"px)";
-}

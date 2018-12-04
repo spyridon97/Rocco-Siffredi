28179d6b71ed9c16047f743408c2a30dfab7f55a
nerd553 <geomelkon@csd.auth.gr>
2018-12-04 17:34:34 +0200

    More responsiveness

--- scripts/responsiveNav.js
+++ scripts/responsiveNav.js
@@ -1,2 +1,23 @@
-const nav = document.getElementsByName('nav')[0];
+let isResponsive = false;
+
+function toggleResponsive() {
+    const header = document.getElementsByTagName("header")[0];
+    const items = document.getElementsByClassName('menu-item');
+    if (isResponsive) {
+        header.className = "";
+        for (let item of items){
+            item.className = "menu-item"
+        }
+        isResponsive = false;
+        return;
+    }
+    
+    header.className = "responsive";
+    for (let item of items){
+        item.className += " responsive"
+    }
+    isResponsive = true;
+    return;
+}
+
 

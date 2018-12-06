150763efe99d6d842679d154df22dd939fbad248
tjgiannhs <36013878+tjgiannhs@users.noreply.github.com>
2018-12-06 22:08:25 +0200

    Added file with multimedia functions

---
+++ scripts/multimedia.js
@@ -0,0 +1,49 @@
+const plaisio = document.getElementById("plaisio");
+const ypotitlos = document.getElementById("ypotitlos");
+
+function change1() {
+  plaisio.src="/images/multimedia/fabi.jpg";
+  plaisio.width="610px";
+  ypotitlos.innerHTML="<span><b>&#160;Shoe Commercial for Fabi&#160;</b></span>";
+}
+    
+    
+
+function change2() {
+  plaisio.src="/images/multimedia/artist.jpg";
+  plaisio.width="405px";
+  ypotitlos.innerHTML="<span><b>&#160;Rocco Siffredi&#160;</b></span>";
+}
+
+
+
+function change3() {
+  plaisio.src="/images/multimedia/family.jpg";
+  plaisio.width="915px";
+  ypotitlos.innerHTML="<span><b>&#160;Rocco Siffredi with his family&#160;</b></span>";
+}
+
+    
+
+function change4() {
+  plaisio.src="/images/multimedia/festival.jpg";
+  plaisio.width="407px";
+  ypotitlos.innerHTML="<span><b>&#160;Rocco Siffredi attends a photocall for 'Rocco' during the 73rd Venice Film Festival at Villa degli Autori on September 5, 2016 in Venice, Italy.&#160;</b></span>";
+}
+     
+    
+
+function change5() {
+  plaisio.src="/images/multimedia/young.jpg";
+  plaisio.width="393px";
+  ypotitlos.innerHTML="<span><b>&#160;Old photo of Rocco Siffredi in his youth&#160;</b></span>";
+}
+
+      
+
+function change6() {
+  plaisio.src="https://www.youtube.com/embed/p2jLFRMjeCg?autoplay=1";
+  plaisio.width="1084px";
+  ypotitlos.innerHTML="<span><b>&#160;Rocco Siffredi arriving at Nice airport for the 2016 Cannes Film festival&#160;</b></span>";
+}
+    
